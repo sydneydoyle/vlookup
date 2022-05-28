d929d90a9bcd2042988a31997fe48026f36258cf
sydneydoyle <106398877+sydneydoyle@users.noreply.github.com>
2022-05-27 23:26:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-import $ from "https://cdn.skypack.dev/jquery@3.6.0";
-import * as bootstrap from "https://cdn.skypack.dev/bootstrap@5.1.3";
-import fontAwesome from "https://cdn.skypack.dev/font-awesome@4.7.0";
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-function init() {
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
-document.addEventListener("DOMContentLoaded", init, false);
